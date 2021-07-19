39b9954a3acc11865e8031c0c458e446c852f216
Shubh <shubhv2@gmail.com>
2021-07-19 15:48:44 +0530

    yo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import Navbar from "./Components/Navbar/navbar";
+import HomePage from "./Components/homepage/homepage";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+
+import Footer from "./Components/footer/footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Redirect path="/" exact to="/home" />
+        </Switch>
+        <Footer />
+      </Router>
+    </>
   );
 }
 
---
+++ src/Components/Navbar/navbar.jsx
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Button from "../button/button";
+import "./navbar.css";
+import img from "../../images/img.png";
+const Navbar = () => {
+  const [click, setClicker] = useState(false);
+  const [button1, setButton] = useState(true);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+  window.addEventListener("resize", showButton);
+  const closeMobileMenu = () => setClicker(false);
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            Hofars
+            <i class="fas fa-radiation-alt ic"></i>
+          </Link>
+          <div className="menu-icon" onClick={() => setClicker(!click)}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link
+                to="/AboutUs"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/resumebank"
+                className="nav-links "
+                onClick={closeMobileMenu}
+              >
+                About
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link to="/ATS" className="nav-links" onClick={closeMobileMenu}>
+                Careers
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/ATS" className="nav-links" onClick={closeMobileMenu}>
+                Contact
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/ATS" className="nav-links" onClick={closeMobileMenu}>
+                Hofars Internship Program
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
